3b617d51d50fcdc0e93c398f02265372a052738a
ersinelmas <elmase23@itu.edu.tr>
2024-03-24 00:07:11 +0300

    customer crud operations added

--- customerservice/src/main/java/com/turkcell/pair8/customerservice/services/abstracts/CustomerService.java
+++ customerservice/src/main/java/com/turkcell/pair8/customerservice/services/abstracts/CustomerService.java
@@ -2,6 +2,7 @@ package com.turkcell.pair8.customerservice.services.abstracts;
 
 import com.turkcell.pair8.customerservice.services.dtos.customer.request.AddCustomerRequest;
 import com.turkcell.pair8.customerservice.services.dtos.customer.request.SearchCustomerRequest;
+import com.turkcell.pair8.customerservice.services.dtos.customer.request.UpdateCustomerRequest;
 import com.turkcell.pair8.customerservice.services.dtos.customer.response.SearchCustomerResponse;
 
 import java.util.List;
@@ -9,4 +10,6 @@ import java.util.List;
 public interface CustomerService {
     List<SearchCustomerResponse> search(SearchCustomerRequest request);
     void add(AddCustomerRequest request);
+    void delete(int id);
+    void update(UpdateCustomerRequest request);
 }
