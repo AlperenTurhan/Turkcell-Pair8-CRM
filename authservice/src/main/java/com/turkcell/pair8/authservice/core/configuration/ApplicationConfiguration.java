986f89184e7322895f85c3c75c9e6c6b209db9b0
ersinelmas <elmase23@itu.edu.tr>
2024-04-04 17:18:50 +0300

    bug fix

--- authservice/src/main/java/com/turkcell/pair8/authservice/core/configuration/ApplicationConfiguration.java
+++ authservice/src/main/java/com/turkcell/pair8/authservice/core/configuration/ApplicationConfiguration.java
@@ -1,9 +1,11 @@
 package com.turkcell.pair8.authservice.core.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 public class ApplicationConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
